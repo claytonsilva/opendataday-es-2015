902ca514ecf3e6c19e8120efc26b9dc27c0dfcf4
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-02-23 08:38:10 -0300

    fazendo load dos dados

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,73 @@
 var express = require('express');
 var router = express.Router();
 
+// todos os dados são carregados no load
+data_2012 = require('../models/2012')();
+data_2013 = require('../models/2013')();
+data_2014 = require('../models/2014')();
+data_total = data_2012.concat(data_2013).concat(data_2014);
+
+
+//realizando reduce para otimizar perfomance em consultas posteriores
+
+
+
+
+function formatData(list){
+        //"orgao":"RÁDIO E TELEVISÃO ESPÍRITO SANTO ",
+        //"favorecido":"CESAN-COMP.ESP.SANTENSE DE SANEAMENTO",
+        //"strdescricaoelementodespesa":"OUTROS SERVICOS DE TERCEIROS-PESSOA JURIDICA",
+        //"strdescricaosubelementodespesa":"SERVICOS DE AGUA E ESGOTO                    ",
+        //"strnomeprograma":"APOIO ADMINISTRATIVO",
+        //"strfonterecurso":"RECURSOS ORDINARIOS                          ",
+        //"strnomeacao":"ADMINISTRACAO DA UNIDADE ",
+        //"datdocumento":"2012-11",
+        //"despesa_repasse":"DESPESA",
+        //"valor_empenhado":0,
+        //"valor_liquidacao":0,
+        //"valor_pago":342.6,
+        //"mes_descritivo":"11 - Novembro",
+        //"ano":2012  
+     //var ret = { "ano" :
+     //}
+    
+}
+
+
 
 router.get('/data/:ano', function(req, res, next) {
     var year = req.param('ano');
     
     if  (year == '2012'){
-        str_load = '../models/2012'
+        res.json(data_2012);
+        
     } else if  (year == '2013'){
-        str_load = '../models/2013'
+        res.json(data_2013);
+        
     }else if  (year == '2014'){
-        str_load = '../models/2014'
+        res.json(data_2014);
+        
     }else{
-        str_load = '../models/2012'
+        res.json(data_total);
+        
     }
-    
-    data = require(str_load)();
-    
-    
-    
-    
-    res.json(data);
-    
-    
-    
 });
+
+
+
+router.get('/data', function(req, res, next) {
+    soma = 0;
+    data_base.forEach(function(el) {
+        soma = soma + el.valor_pago;
+    });
+    
+    res.json(data_total);
+});
+
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
